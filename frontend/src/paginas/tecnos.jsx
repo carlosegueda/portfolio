9e76e260534fec36af44590de83b9b82c44b99c3
Carlos Osegueda <carlosegueda200@gmail.com>
2025-08-25 01:08:02 -0600

    mejora de tecnos

--- frontend/src/paginas/tecnos.jsx
+++ frontend/src/paginas/tecnos.jsx
@@ -1,10 +1,19 @@
 import Navbar from "../componentes/Navbar";
+import "../css/tecnos.css";
 
 export default function PaginaTecnologias() {
   return (
     <>
       <Navbar />
-      <h1>TECNOLOYIA</h1>
+      <div className="panelTecnos">
+        <div className="tecno"> <img src="hml5.png"/><h1>HTML</h1></div>
+        <div className="tecno"> <img src="hml5.png"/><h1>JAVASCRIPT</h1></div>
+        <div className="tecno"> <img src="hml5.png"/><h1>CSS</h1></div>
+        <div className="tecno"> <img src="hml5.png"/><h1>PYTHON</h1></div>
+        <div className="tecno"> <img src="hml5.png"/><h1>REACT</h1></div>
+        <div className="tecno"> <img src="hml5.png"/><h1>TAILWIND</h1></div>
+ 
+      </div>
     </>
   );
 }
