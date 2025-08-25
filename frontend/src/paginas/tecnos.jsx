e9076984ed4b20438202eeaa87c73392d081739a
Carlos Osegueda <carlosegueda200@gmail.com>
2025-08-25 02:35:59 -0600

    mejoras

--- frontend/src/paginas/tecnos.jsx
+++ frontend/src/paginas/tecnos.jsx
@@ -5,14 +5,16 @@ export default function PaginaTecnologias() {
   return (
     <>
       <Navbar />
+      <div className="titulo"> <h1>TECNOLOGÍAS</h1></div>
       <div className="panelTecnos">
-        <div className="tecno"> <img src="hml5.png"/><h1>HTML</h1></div>
-        <div className="tecno"> <img src="hml5.png"/><h1>JAVASCRIPT</h1></div>
-        <div className="tecno"> <img src="hml5.png"/><h1>CSS</h1></div>
-        <div className="tecno"> <img src="hml5.png"/><h1>PYTHON</h1></div>
-        <div className="tecno"> <img src="hml5.png"/><h1>REACT</h1></div>
-        <div className="tecno"> <img src="hml5.png"/><h1>TAILWIND</h1></div>
- 
+        <div className="tecno1"> <img src="htmlN.png"/><h1>HTML</h1></div>
+        <div className="tecno2"> <img src="jsN.png"/><h1>JS</h1></div>
+        <div className="tecno3"> <img src="cssN.png"/><h1>CSS</h1></div>
+        <div className="tecno4"> <img src="pyN.png"/><h1>PYTHON</h1></div>
+        <div className="tecno5"> <img src="reactN.png"/><h1>REACT</h1></div>
+        <div className="tecno6"> <img src="tailwindN.png"/><h1>TAILWIND</h1></div>
+        <div className="tecno5"> <img src="reactN.png"/><h1>REACT</h1></div>
+        <div className="tecno6"> <img src="tailwindN.png"/><h1>TAILWIND</h1></div>
       </div>
     </>
   );
