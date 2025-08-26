67d535aa4d66bf1b164c0d3880d4502dbc2d9912
Carlos Osegueda <carlosegueda200@gmail.com>
2025-08-26 14:54:12 -0600

    modif

--- frontend/src/paginas/projects.jsx
+++ frontend/src/paginas/projects.jsx
@@ -5,7 +5,28 @@ export default function PaginaProyectos() {
     <>
       <Navbar />
       <h1>PROYECTOS</h1>
-      
+
+      <form
+        name="contact"
+        netlify
+        netlify-honeypot="bot-field"
+        action="/projects"
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <input type="hidden" name="to" value="carlosegueda2002@gmail.com" />
+
+        <p hidden>
+          <label>
+            No llenar: <input name="bot-field" />
+          </label>
+        </p>
+
+        <input type="text" name="nombre" placeholder="Nombre" required />
+        <input type="email" name="email" placeholder="Email" required />
+        <textarea name="mensaje" placeholder="Mensaje" required></textarea>
+
+        <button type="submit">Enviar</button>
+      </form>
     </>
   );
 }
